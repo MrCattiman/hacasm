ea43889d23ee188ca2abab031311ee72c5befc68
MrCattiman <commonsay@gmail.com>
2021-01-09 22:47:14 -0600

    fixed inbuflen

--- src/hacal_asmlib.h
+++ src/hacal_asmlib.h
@@ -1,2 +1,2 @@
 extern char** hacal_loadSet(char* filepath);
-extern char*  hacal_asm(char** cset, char** vset, char* inbuf, char* inbuflen);
+extern char*  hacal_asm(char** cset, char** vset, char* inbuf, unsigned int* inbuflen);

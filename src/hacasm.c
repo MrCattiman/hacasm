6775715eb80ef444bdeac9dc16267487f44a5a6c
MrCattiman <commonsay@gmail.com>
2021-01-09 22:47:28 -0600

    Added the ability to set the input file

--- src/hacasm.c
+++ src/hacasm.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include "hacal_asmlib.h"
 
 char** cset;
 char** vset;
 char* input;
-char* output;
 
 void previewSet() {
 	printf("----------\nConsonants\n----------\n\nHex | Value\n--- | -----\n");
@@ -18,17 +19,39 @@ void previewSet() {
 	};
 };
 
+void setIn(char* path) {
+	if (input != 0) free(input);
+	FILE* inputFile = fopen(path,"r");
+	if (inputFile == 0) {
+		fprintf(stderr,"Null input file pointer.\n");
+		exit(1);
+	};
+	fseek(inputFile, 0L, SEEK_END);
+	unsigned int size = ftell(inputFile);
+	rewind(inputFile);
+	input = (char*)malloc(size);
+	fread(input, size, 1, inputFile);
+	fclose(inputFile);
+};
+
 int main(int args, char *arg[]) {
 	if (args < 3) {
-		printf("hacasm <consonant set path> <vowel set path> [<parameter> (path)]\n\n-i <file path> or --input <file path>\n	Set the input file, must come before -t or -d.\n-t <file path> or --text <file path>\n	Output a basic HACAL text file.\n-d <file path> or --dict <file path>\n	Output a dictionary.\n-o <file path> or --out <file path>\n	output to a file rather than stdout, must come before -t or -d.\n-h <file path> or --headless <file path>\n	generate the selected file(s) without a header.\n--preview-set\n	Preview/test the set you would use.");
+		printf("hacasm <consonant set path> <vowel set path> [<parameter> (path)]\n\n-i <file path> or --input <file path>\n	Set the input file, must come before -t or -d.\n-t <file path> or --text <file path>\n	Output a basic HACAL text file.\n-d <file path> or --dict <file path>\n	Output a dictionary.\n-h <file path> or --headless <file path>\n	generate the selected file(s) without a header.\n--preview-set\n	Preview/test the set you would use.");
 		return 0;
 	};
 	cset = hacal_loadSet(arg[1]);
 	vset = hacal_loadSet(arg[2]);
 	if (args == 3) previewSet();
 	for (int i = 3; i < args; i++) {
-		if(arg[i] == "--preview-set") {
+		if ( strcmp(arg[i], "--preview-set") == 0) {
 			previewSet();
+		};
+		if ( strcmp(arg[i], "--input") == 0) {
+			setIn(arg[++i]);
+			printf("\n%s",input);
+		};
+		if ( strcmp(arg[i], "--text") == 0 ) {
+			hacal_asm();
 		};
 	};
 };

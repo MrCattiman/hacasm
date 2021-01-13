d9c974271c8b39207f431ba1b5244008a7ef1af8
MrCattiman <commonsay@gmail.com>
2021-01-12 19:30:41 -0600

    IT FINALLY WORKS

--- src/hacal_asmlib.c
+++ src/hacal_asmlib.c
@@ -4,6 +4,8 @@
 #include "ctype.h"
 #include "string.h"
 
+#define IBSPACE inbuf[i] == ' ' || inbuf[i] == '\n' || inbuf[i] == 0
+
 char** hacal_loadSet(char* filepath) {
 	FILE* file = fopen(filepath,"r");
 	char** set = malloc(sizeof(char*) * 15);
@@ -25,7 +27,7 @@ char** hacal_loadSet(char* filepath) {
 		};
 		if (get != EOF) {
 			if (bufi < 10) {
-				set[i][bufi] = get;
+				set[i][bufi] = tolower(get);
 				bufi++;
 			};
 	 		goto loop;
@@ -34,17 +36,73 @@ char** hacal_loadSet(char* filepath) {
 	return set;
 }
 
+/* Outputs the string id with the best match */
+int hacallib_strfirst(char* str, unsigned char strc, char** strs) {
+	int i = 0;
+	int xi[strc + 1];
+	for (unsigned int ii = strc; ii--; ) {
+		for ( i = 0; tolower(str[i]) == strs[ii][i]; i++ );
+		if ( strs[ii][i] == 0 ) i++;
+		xi[ii] = i;
+	};
+	int best = -1;
+	for ( i = strc; i--; ) if ( xi[i] > ( best > -1 ? xi[best] : -1 ) ) best = i;
+	return best;
+}
+
 /* The sets should be loaded in memory as an array with pointers to arrays of chars */
 char* hacal_asm(char** cset, char** vset, char* inbuf, unsigned int inbuflen) {
-	char buf[0xff];
-	char          syl    = 0;
-	char          buft   = 0;
-	unsigned int  oi     = 0;
-	char*         outbuf = (char*)calloc(inbuflen, sizeof(char));
-	char          parity = 0;
+	unsigned char  syl       = 0;
+	unsigned int   obuft     = 0;
+	unsigned char* outbuf    = (char*)calloc(inbuflen, sizeof(char));
+	char           csets[15];
+	char           vsets[15];
+	/* String comparison size; what size the string must be before it is compared to the sets. */
 	if (outbuf == 0) {
 		fprintf(stderr, "Failed to allocate memory for a buffer.\n");
 		exit(2);
 	};
-	return outbuf;
+	/* Calculate set entry sizes */
+	for (unsigned int i = 15; i--;) {
+		unsigned int ii = 0;
+		while (cset[i][++ii] != 0);
+		csets[i] = ii;
+	};
+	for (unsigned int i = 15; i--;) {
+		unsigned int ii = 0;
+		while (vset[i][++ii] != 0);
+		vsets[i] = ii;
+	};
+	unsigned int i = 0;
+	int s = 0;
+	while (i < inbuflen) {
+		while ( IBSPACE ) i++;
+		s = hacallib_strfirst(inbuf + i, 15, cset);
+		if ( s > -1 ) {
+			i   += csets[s];
+			syl |= ( s + 1 ) << 4;
+		};
+		s = hacallib_strfirst(inbuf + i, 15, vset);
+		if ( s > -1 ) {
+			i   += vsets[s];
+			syl |= s + 1;
+		};
+		if (syl != 0) outbuf[obuft++] = syl;
+		syl = 0;
+		if ( IBSPACE && outbuf[obuft] != 0 ) {
+			outbuf[obuft++] = 0;
+			i++;
+		};
+	};
+	for ( i = 0; outbuf[i++] != 0 || outbuf [i] != 0; );
+	unsigned char* rbuf = (unsigned char*)malloc(i);
+	if (rbuf == 0) {
+		fprintf(stderr, "Failed to allocate memory for a buffer.\n");
+		exit(2);
+	};
+	while ( i-- ) {
+		rbuf[i] = outbuf[i];
+	};
+	free(outbuf);
+	return rbuf;
 }

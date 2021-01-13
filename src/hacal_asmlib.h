c9fbcf7d4fd34a35dfaf72de809d6da6f9fd93de
MrCattiman <commonsay@gmail.com>
2021-01-12 19:55:32 -0600

    Small fix

--- src/hacal_asmlib.h
+++ src/hacal_asmlib.h
@@ -1,2 +1,3 @@
 extern char** hacal_loadSet(char* filepath);
+extern unsigned int hacallib_strfirst(char* str, unsigned char strc, char** strs);
 extern char*  hacal_asm(char** cset, char** vset, char* inbuf, unsigned int inbuflen);
